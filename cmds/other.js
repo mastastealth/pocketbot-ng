6aa7bf3428eab43356b3147aff5c4a030752e066
Brian Franco <mastastealth+git@gmail.com>
2021-05-27 20:01:45 -0400

    !verify restored

--- cmds/other.js
+++ cmds/other.js
@@ -1,33 +1,33 @@
 const stripIndents = require("common-tags").stripIndents;
 
 module.exports = (bot) => {
-	const { vars: x, helpers } = bot.PB;
+  const { vars: x, helpers } = bot.PB;
 
-	bot.registerCommand("8ball", (msg) => {
-		let n = Math.floor(Math.random() * (20) - 1),
-			answer = [
-				"It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
-				"As I see it, yes", "Most likely", "Outlook good", "Yes",
-				"Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
-				"Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
-				"My sources say no", "Outlook not so good", "Very doubtful"
-			];
+  bot.registerCommand("8ball", (msg) => {
+    let n = Math.floor(Math.random() * (20) - 1),
+      answer = [
+        "It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
+        "As I see it, yes", "Most likely", "Outlook good", "Yes",
+        "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
+        "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
+        "My sources say no", "Outlook not so good", "Very doubtful"
+      ];
 
-		msg.channel.createMessage(`:8ball: says _"${answer[n]}"_`,);
-	}, {
-		description: "Seek the wisdom of PB"
-	});
+    msg.channel.createMessage(`:8ball: says _"${answer[n]}"_`,);
+  }, {
+    description: "Seek the wisdom of PB"
+  });
 
-	bot.registerCommand("lsroles", (msg) => {
-		console.log(msg.member.roles)
-	}, {
+  bot.registerCommand("lsroles", (msg) => {
+    console.log(msg.member.roles)
+  }, {
     description: "Print member roles into console, only for mods and up",
-		requirements: {
-			custom(msg) {
+    requirements: {
+      custom(msg) {
         return helpers.hasModPerms(msg.member.roles);
       }
-		}
-	});
+    }
+  });
 
   bot.registerCommand("dontask", (msg) => {
     msg.channel.createMessage("You shouldn't be asking about this. :eyes: <https://www.twitch.tv/toothandtailtv/clip/CarefulGlamorousWatercressRuleFive>");
@@ -96,7 +96,13 @@ module.exports = (bot) => {
     }
   });
 
-	return {
-		register() { console.info("Registered other commands."); }
-	};
+  bot.registerCommand("verify", (msg) => {
+    msg.channel.createMessage(":white_check_mark: Click here to verify your TnT files: http://toothandtailgame.com/verify")
+  }, {
+    description: "Posts a Steam link which automatically verifies the TnT directory"
+  });
+
+  return {
+    register() { console.info("Registered other commands."); }
+  };
 };
