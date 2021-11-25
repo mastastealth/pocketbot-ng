713edd75061de6678737a05c854fcc6c50247d93
Brian Franco <mastastealth+git@gmail.com>
2021-11-24 20:56:38 -0500

    Bank commands get slashed

--- cmds/admin.js
+++ cmds/admin.js
@@ -1,69 +1,119 @@
 module.exports = (bot) => {
   const { vars: x, main, helpers } = bot.PB;
 
-  bot.registerCommand("say", (msg) => {
-    bot.createMessage(x.chan, msg.content.replace("!say ", ""));
-  }, {
-    description: "Allows admin to speak as PB",
-    requirements: {
-      roleIDs: [x.admin]
+  bot.registerCommand(
+    "say",
+    (msg) => {
+      bot.createMessage(x.chan, msg.content.replace("!say ", ""));
+    },
+    {
+      description: "Allows admin to speak as PB",
+      requirements: {
+        roleIDs: [x.admin],
+      },
     }
-  });
+  );
 
-  bot.registerCommand("rename", async (msg, args) => {
-    if (!msg.mentions.length && args.length !== 2) {
-      msg.channel.createMessage("No user detected. Enter the user via an @ mention or by ID.");
-      return false;
-    }
+  // bot.PB.slashCmds.push({
+  //   info: {
+  //     name: "say",
+  //     description: "Allows admin to speak as PB",
+  //     options: [
+  //       {
+  //         name: "channel",
+  //         description: "Select a channel to speak in.",
+  //         type: 7,
+  //         required: true,
+  //       },
+  //       {
+  //         name: "message",
+  //         description: "Message you want to PB to repeat.",
+  //         type: 3,
+  //         required: true,
+  //       }
+  //     ],
+  //   },
+  //   cmd(action) {
+  //     console.log(action)
+  //     bot.createMessage(res);
+  //   },
+  // });
 
-    const target = msg.mentions.length 
-      ? await bot.guilds.get(x.chan).members.get(msg.mentions[0].id) 
-      : await bot.guilds.get(x.chan).members.get(args[0]);
-    const prevName = target && target.nick ? target.nick : "N/A"; // Return current nickname of member
-
-    if (target) {
-      target.edit({
-        nick: args.slice(1).join(" ")
-      }, "Renamed user because his name probably sucked.");
-  
-      msg.delete();
-      main.modEmbed({
-        admin: msg.author,
-        action: "rename",
-        icon: ":crayon:",
-        user: target.id,
-        prevName,
-        bot
-      });
-    }
-  }, {
-    description: "Allows mod/admin to rename a user",
-    requirements: {
-      custom(msg) {
-        return helpers.hasModPerms(msg.member.roles);
+  bot.registerCommand(
+    "rename",
+    async (msg, args) => {
+      if (!msg.mentions.length && args.length !== 2) {
+        msg.channel.createMessage(
+          "No user detected. Enter the user via an @ mention or by ID."
+        );
+        return false;
       }
-    }
-  });
 
-  bot.registerCommand("purge", async (msg, args) => {
-    try {
-      const messages = await msg.channel.getMessages({ limit: parseInt(args[0]) });
-      const mIDs = messages.map(m => m.id);
-    
-      await bot.deleteMessages(msg.channel.id, mIDs, "Purging some stuff with bot.");
-    } catch(e) {
-      msg.channel.createMessage(`🕑 \`${e}\``);
-    }
-  }, {
-    description: "Delete a number of messages.",
-    requirements: {
-      custom(msg) {
-        return helpers.hasModPerms(msg.member.roles);
+      const target = msg.mentions.length
+        ? await bot.guilds.get(x.chan).members.get(msg.mentions[0].id)
+        : await bot.guilds.get(x.chan).members.get(args[0]);
+      const prevName = target && target.nick ? target.nick : "N/A"; // Return current nickname of member
+
+      if (target) {
+        target.edit(
+          {
+            nick: args.slice(1).join(" "),
+          },
+          "Renamed user because his name probably sucked."
+        );
+
+        msg.delete();
+        main.modEmbed({
+          admin: msg.author,
+          action: "rename",
+          icon: ":crayon:",
+          user: target.id,
+          prevName,
+          bot,
+        });
       }
+    },
+    {
+      description: "Allows mod/admin to rename a user",
+      requirements: {
+        custom(msg) {
+          return helpers.hasModPerms(msg.member.roles);
+        },
+      },
     }
-  });
+  );
 
-  return { 
-    register() { console.info("Registered admin commands."); }
+  bot.registerCommand(
+    "purge",
+    async (msg, args) => {
+      try {
+        const messages = await msg.channel.getMessages({
+          limit: parseInt(args[0]),
+        });
+        const mIDs = messages.map((m) => m.id);
+
+        await bot.deleteMessages(
+          msg.channel.id,
+          mIDs,
+          "Purging some stuff with bot."
+        );
+      } catch (e) {
+        msg.channel.createMessage(`🕑 \`${e}\``);
+      }
+    },
+    {
+      description: "Delete a number of messages.",
+      requirements: {
+        custom(msg) {
+          return helpers.hasModPerms(msg.member.roles);
+        },
+      },
+    }
+  );
+
+  return {
+    register() {
+      console.info("Registered admin commands.");
+    },
   };
 };
--- cmds/bank.js
+++ cmds/bank.js
@@ -1,66 +1,99 @@
 module.exports = (bot) => {
-  const { 
-    vars: x, 
-    // main, 
-    helpers, 
-    fb 
-  } = bot.PB;
+  const { vars: x, helpers, fb } = bot.PB;
 
-  bot.registerCommand("wip", async (msg) => {
-    const total = await fb.getProp(msg.author.id, "currency");
-    msg.channel.createMessage(`My records say you have **${total || "no"}** ${x.emojis.wip} coins`);
-  }, {
-    description: "Check the amount of WIP in your account"
+  bot.PB.slashCmds.push({
+    info: {
+      name: "wip",
+      description: "Check the amount of WIP in your account",
+    },
+    async cmd(action) {
+      const total = await fb.getProp(action.member.id, "currency");
+      action.createMessage(
+        `My records say you have **${total || "no"}** ${x.emojis.wip} coins`
+      );
+    },
   });
 
-  bot.registerCommand("transfer", async (msg, args) => {
-    const who = helpers.getUser(args?.[0]);
-    const give = parseInt(args[1]);
+  bot.PB.slashCmds.push({
+    info: {
+      name: "transfer",
+      description: "Transfers an amount of WIP to another user",
+      options: [
+        {
+          name: "user",
+          description: "Who you want to transfer to.",
+          type: 6,
+          required: true,
+        },
+        {
+          name: "amount",
+          description: "How much you want to transfer.",
+          type: 4,
+          required: true,
+        },
+      ],
+    },
+    async cmd(action) {
+      const who = helpers.getUser(action.data.options[0].value);
+      const give = parseInt(action.data.options[1].value);
 
-    if (args.length !== 2) {
-      return msg.channel.createMessage('Please tell me which `@user` you want to transfer `N` amount to...');
-    }
+      if (give > 0) {
+        const have = await fb.getProp(action.member.id, "currency");
 
-    if (give > 0) {
-      const have = await fb.getProp(msg.author.id, "currency");
-
-      if (have && have >= give) {
-        fb.setProp(who, "currency", give);
-        fb.setProp(msg.author.id, "currency", give * -1);
-        msg.channel.createMessage(`**${give}** ${x.emojis.wip} coins successfully transferred.`);
+        if (have && have >= give) {
+          fb.setProp(who, "currency", give);
+          fb.setProp(action.member.id, "currency", give * -1);
+          return action.createMessage(
+            `**${give}** ${x.emojis.wip} coins successfully transferred.`
+          );
+        } else {
+          return action.createMessage(
+            `Transfer denied. Cannot give **${give}** ${x.emojis.wip} when you only have **${have}** ${x.emojis.wip}.`
+          );
+        }
       } else {
-        msg.channel.createMessage(`Transfer denied. Cannot give **${give}** ${x.emojis.wip} when you only have **${have}** ${x.emojis.wip}.`);
+        return action.createMessage(
+          "Transfer unsuccessful. Syntax is `!transfer @user N`."
+        );
       }
-    } else {
-      msg.channel.createMessage('Transfer unsuccessful. Syntax is `!transfer @user N`.');
-    }
-  }, {
-    description: "Transfers an amount of WIP to another user"
+    },
   });
 
-  bot.registerCommand("give", (msg, args) => {
-    const give = parseInt(args[1]);
+  bot.registerCommand(
+    "give",
+    (msg, args) => {
+      const give = parseInt(args[1]);
 
-    if (args.length !== 2) {
-      return msg.channel.createMessage('Please tell me which `@user` you want to give `N` amount to...');
-    }
-
-    if (give > 0) {
-      fb.setProp(helpers.getUser(args[0]), "currency", give);
-      msg.channel.createMessage(`**${give}** ${x.emojis.wip} coins successfully bestowed.`);
-    } else {
-      msg.channel.createMessage('Give amount is not a number. Please learn your maths.');
-    }
-  }, {
-    description: "Admin only command to spur inflation of the WIP economy",
-    requirements: {
-      custom(msg) {
-        return helpers.hasModPerms(msg.member.roles);
+      if (args.length !== 2) {
+        return msg.channel.createMessage(
+          "Please tell me which `@user` you want to give `N` amount to..."
+        );
       }
-    }
-  });
 
-  return { 
-    register() { console.info("Registered banking commands."); }
+      if (give > 0) {
+        fb.setProp(helpers.getUser(args[0]), "currency", give);
+        msg.channel.createMessage(
+          `**${give}** ${x.emojis.wip} coins successfully bestowed.`
+        );
+      } else {
+        msg.channel.createMessage(
+          "Give amount is not a number. Please learn your maths."
+        );
+      }
+    },
+    {
+      description: "Admin only command to spur inflation of the WIP economy",
+      requirements: {
+        custom(msg) {
+          return helpers.hasModPerms(msg.member.roles);
+        },
+      },
+    }
+  );
+
+  return {
+    register() {
+      console.info("Registered banking commands.");
+    },
   };
 };
